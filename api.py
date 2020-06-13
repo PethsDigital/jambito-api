6ec47954be0ca7a0834c186f216d06942a2d2966
olumidayy <niolumi4eva@gmail.com>
2020-06-13 12:12:46 +0100

    added search by subject and course functionality

---
+++ api.py
@@ -0,0 +1,65 @@
+from data import getData
+from flask import Flask  # jsonify
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+data = getData()
+
+
+def searchByCourse(search):
+    data, search = getData(), search.upper()
+    realData = data['results']
+    # print(str(data), realData)
+    courses = realData.keys()
+    data = {
+        'results': {
+            course: realData[course]
+            for course in courses
+            if search in course
+        }
+    }
+    print(type(str(data)))
+    return data
+
+
+def searchBySubject(search):
+    data = getData()
+    realData = data['results']
+    courses = realData.keys()
+    data = {
+        'results': {
+            course: realData[course]
+            for course in courses
+            if search.lower() in str(realData[course]).lower()
+        }
+    }
+    return data
+
+
+class Index(Resource):
+    def get(self):
+        return data
+
+
+class SeearchByCourse(Resource):
+
+    def get(self, search):
+        data = searchByCourse(search)
+        return data
+
+
+class SeearchBySubject(Resource):
+
+    def get(self, search):
+        data = searchBySubject(search)
+        print(data)
+        return data
+
+
+api.add_resource(Index, '/')
+api.add_resource(SeearchByCourse, '/course/<string:search>')
+api.add_resource(SeearchBySubject, '/subject/<string:search>')
+
+if __name__ == '__main__':
+    app.run()
--- data.py
+++ data.py
@@ -1,30 +1,28 @@
 from openpyxl import load_workbook
-
+# from openpyxl.styles import PatternFill, Color
+# from openpyxl.styles import NamedStyle
 
 excel_file = 'jambito.xlsx'
-wb = load_workbook(excel_file, data_only=True)
-sh = wb.get_sheet_names()
-jamb, olevel = wb[sh[1]], wb[sh[0]]
+workbook = load_workbook(excel_file, data_only=True)
+worksheets = workbook.get_sheet_names()
+jamb, olevel = workbook[worksheets[1]], workbook[worksheets[0]]
 
 
 def getHex(cell):
     return cell.fill.start_color.index
 
 
-for i in range(1, 665):
-    print(getHex(jamb[f'A{i}']))
-
 codeMap = {olevel[f'B{i}'].value: olevel[f'A{i}'].value for i in range(2, 28)}
 
 
-def getCodes(row):
+def getSubjects(row):
     hexes = set([getHex(cell) for cell in row if cell.value is not None])
     comp, ops, others = [], [], []
     for hx in hexes:
         if hx == '00000000' or hx == 'FFFFFFFF':
             comp.extend(
                 [codeMap[cell.value] for cell in row if getHex(
-                    cell) == '00000000' and isinstance(cell.value, float)])
+                    cell) == hx and isinstance(cell.value, float)])
         else:
             subs = [codeMap[cell.value] for cell in row if getHex(
                 cell) == hx and isinstance(cell.value, float)]
@@ -46,7 +44,7 @@ def getData():
     data = {}
 
     for row in range(2, 665):
-        data[jamb[f'N{row}'].value] = getCodes(jamb[row])
+        data[jamb[f'O{row}'].value] = getSubjects(jamb[row])
     return {
         'results': data
     }
