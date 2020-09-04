a3a63679916d164fb1b80c7fcaf379b10cd9532f
olumidayy <niolumi4eva@gmail.com>
2020-09-04 11:13:42 +0100

    ff

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from data import getData
 from flask import Flask
 from flask_restful import Resource, Api
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
 # CORS(app)
